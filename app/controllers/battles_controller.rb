54db842c0353534248ff0742bd2950e9efc090da
Michael Wynholds <mike@carbonfive.com>
2013-09-13 10:53:07 -0700

    Fixed some small stuff

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -47,7 +47,7 @@ class BattlesController
   def show(id)
     battle = Battle.find_by_id id
     return [404, "Unknown battle: #{id}"] unless battle
-    json = jbuilder :"battle/turn", locals: { battle: battle }
+    json = jbuilder :"battle/show", locals: { battle: battle }
     erb :"battle/show", locals: { battle: battle, json: json, active_nav: 'arena' }
   end
 
