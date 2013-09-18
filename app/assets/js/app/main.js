e26d86f2822b2a774db1dda488931f576d8f952d
David Hendee <david@carbonfive.com>
2013-09-17 22:05:16 -0700

    autoplay feature. to turn on, visit /battles/autoplay/on. turn off sim.

--- app/assets/js/app/main.js
+++ app/assets/js/app/main.js
@@ -4,14 +4,41 @@ $(document).ready(function() {
     markedDown.html(markdown.toHTML(markedDown.html()));
   });
 
-  $('#create-battle .random').on('click', function() {
-    $('#create-battle select').each(function() {
-      var options = $(this).find('option');
+  if ($('#create-battle').length > 0) {
+    $('#create-battle .random').on('click', function() {
+      var selects = $('#create-battle select');
+      selects.each(function(i) {
+        var select = $(this);
+        var options = select.find('option');
+        if (i > 0) {
+          var previousSelection = selects.eq(i - 1).find(':selected').val();
+          randomizeOptions(options, previousSelection);
+        } else {
+          randomizeOptions(options, null);
+        }
+      });
+      return true;
+    });
+
+    function randomizeOptions(options, forbiddenValue) {
       if (options.length > 1) {
         options.removeAttr('selected');
         options.eq(Math.floor(Math.random() *  options.length)).attr('selected', 'selected');
+        if (forbiddenValue && options.parent().find(':selected').val() == forbiddenValue) {
+          randomizeOptions(options, forbiddenValue);
+        }
       }
-    });
-    return true;
-  });
+    }
+
+    if ($('body').hasClass('autoplay')) {
+      $('#create-battle .random').trigger('click');
+    }
+  }
+
+  if ($('.leaderboard').length > 0 && $('body').hasClass('autoplay')) {
+    setTimeout(function() {
+      window.location = '/battles/new';
+    }, 5000);
+  }
+
 });
