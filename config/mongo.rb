dc11b2df036c62a40978134609fb72a7a2596fe3
Christian Nelson <christian@carbonfive.com>
2013-09-21 08:48:06 -0700

    Increase the mongo pool size.

--- config/mongo.rb
+++ config/mongo.rb
@@ -1,4 +1,4 @@
 require 'mongo_mapper'
 
-MongoMapper.connection = Mongo::Connection.new 'localhost'
+MongoMapper.connection = Mongo::Connection.new('localhost', :pool_size => 5, :pool_timeout => 5)
 MongoMapper.database = 'kablammo_development'
