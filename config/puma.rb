5a404ba0ce1236a04a9d639817c507fd223c160c
Will Ockelmann-Wagner <wow@carbonfive.com>
2019-08-13 11:58:59 -0700

    config tweaks

--- config/puma.rb
+++ config/puma.rb
@@ -3,8 +3,8 @@
 threads 4, 16
 
 bind 'tcp://0.0.0.0:9292'
-bind 'unix://var/run/puma.sock'
-pidfile 'var/run/puma.pid'
+bind 'unix://shared/tmp/sockets/puma.sock'
+pidfile 'shared/tmp/pids/puma.pid'
 
 on_restart do
   # TODO redis
