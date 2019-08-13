bf192d23817ea6020f6b8d729d178c6001a4ccbc
Will Ockelmann-Wagner <wow@carbonfive.com>
2019-08-13 13:44:52 -0700

    trying adding logging

--- index.rb
+++ index.rb
@@ -9,17 +9,34 @@ EOF
   exit 1
 end
 
-require 'rubygems'
-require 'bundler'
+require "rubygems"
+require "bundler"
 Bundler.require :default
 
-require 'sinatra/base'
-require 'sinatra/flash'
-require 'sprockets'
+require "sinatra/base"
+require "sinatra/flash"
+require "sprockets"
+require 'logger'
 
 class KablammoServer < Sinatra::Base
+  ::Logger.class_eval { alias :write :'<<' }
+
+  access_log = ::File.join(::File.dirname(::File.expand_path(__FILE__)), "log", "access.log")
+  access_logger = ::Logger.new(access_log)
+
+  error_logger = ::File.new(::File.join(::File.dirname(::File.expand_path(__FILE__)), "log", "error.log"), "a+")
+  error_logger.sync = true
+
+  configure do
+    use ::Rack::CommonLogger, access_logger
+  end
+ 
+  before {
+    env["rack.errors"] =  error_logger
+  }
+
   set :root, File.dirname(__FILE__)
-  set :bind, '0.0.0.0'
+  set :bind, "0.0.0.0"
 
   # initialize new sprockets environment
   set :environment, Sprockets::Environment.new
@@ -37,19 +54,19 @@ class KablammoServer < Sinatra::Base
   register Sinatra::Flash
 end
 
-Dir['./ext/**/*.rb'].each { |file| require file }
+Dir["./ext/**/*.rb"].each { |file| require file }
 
 Mongoid.load!("config/mongoid.yml")
 
-require './app/models/target'
-require './app/models/board' # need this before Battle
+require "./app/models/target"
+require "./app/models/board" # need this before Battle
 
-Dir['./app/models/**/*.rb'].each { |file| require file }
+Dir["./app/models/**/*.rb"].each { |file| require file }
 
-Dir['./app/controllers/*.rb'].each { |file| require file }
+Dir["./app/controllers/*.rb"].each { |file| require file }
 
-require './app/engine/turn_handler'
-Dir['./app/engine/*.rb'].each { |file| require file }
+require "./app/engine/turn_handler"
+Dir["./app/engine/*.rb"].each { |file| require file }
 
-require './config/strategy'
-require './config/routes'
+require "./config/strategy"
+require "./config/routes"
