57793ed7a3582f3647ee584974372655ccea2d9a
Will Ockelmann-Wagner <wow@carbonfive.com>
2019-08-13 15:33:33 -0700

    remove logging, puma is handling it

--- index.rb
+++ index.rb
@@ -16,25 +16,8 @@ Bundler.require :default
 require "sinatra/base"
 require "sinatra/flash"
 require "sprockets"
-require 'logger'
 
 class KablammoServer < Sinatra::Base
-  ::Logger.class_eval { alias :write :'<<' }
-
-  access_log = ::File.join(::File.dirname(::File.expand_path(__FILE__)), "log", "access.log")
-  access_logger = ::Logger.new(access_log)
-
-  error_logger = ::File.new(::File.join(::File.dirname(::File.expand_path(__FILE__)), "log", "error.log"), "a+")
-  error_logger.sync = true
-
-  configure do
-    use ::Rack::CommonLogger, access_logger
-  end
- 
-  before {
-    env["rack.errors"] =  error_logger
-  }
-
   set :root, File.dirname(__FILE__)
   set :bind, "0.0.0.0"
 
