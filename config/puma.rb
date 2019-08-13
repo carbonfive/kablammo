5c5e00662daa788349ab92bf74697c5132ddd864
Will Ockelmann-Wagner <wow@carbonfive.com>
2019-08-13 11:45:20 -0700

    nginx conf

--- config/puma.rb
+++ config/puma.rb
@@ -1,18 +1,10 @@
 #!/usr/bin/env puma
 
-# Configure “min” to be the minimum number of threads to use to answer
-# requests and “max” the maximum.
-#
-# The default is “0, 16”.
-#
 threads 4, 16
 
-# Bind the server to “url”. “tcp://”, “unix://” and “ssl://” are the only
-# accepted protocols.
-#
-# The default is “tcp://0.0.0.0:9292”.
-#
 bind 'tcp://0.0.0.0:9292'
+bind 'unix://var/run/puma.sock'
+pidfile 'var/run/puma.pid'
 
 on_restart do
   # TODO redis
